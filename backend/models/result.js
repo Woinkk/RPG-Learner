a7edc1702164f98509e214f761b21081d2a85644
joseph <jeanne@intechinfo.fr>
2020-04-09 11:46:35 +0200

    insert school, result and class

--- backend/models/result.js
+++ backend/models/result.js
@@ -1,5 +1,17 @@
-// const PostgresStore = require('../utils/PostgresStore.js');
+const PostgresStore = require('../utils/PostgresStore.js');
+
 class result {
+  static async insert (json) {
+    const results = await PostgresStore.pool.query({
+      text: `
+            INSERT INTO ${result.tableName}
+            (goodanswers, idstudent, idquizz) VALUES($1, $2, $3)
+            `,
+      values: [json.goodanswers, json.idstudent, json.idquizz]
+    });
+    return results;
+  }
+
   static toSqlTable () {
     return ` 
           CREATE TABLE ${result.tableName}(
