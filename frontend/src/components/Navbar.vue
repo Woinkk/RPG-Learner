89802dd7e5ab8da3bd9a0eb11e9522b41e87a0b4
thomas <bury@intechinfo.fr>
2020-04-15 09:44:51 +0200

    Frontend Login and creation  navbar component

---
+++ frontend/src/components/Navbar.vue
@@ -0,0 +1,29 @@
+<template>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+     
+</template>
+
+<script>
+export default {
+    name: "Navbar"
+}
+</script>
