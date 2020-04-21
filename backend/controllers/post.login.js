cfefe04665bf914a6287f191395324e8e3d87a85
thomas <bury@intechinfo.fr>
2020-04-21 15:41:06 +0200

    Login student

--- backend/controllers/post.login.js
+++ backend/controllers/post.login.js
@@ -1,10 +1,11 @@
 const Teacher = require('../models/teacher.js');
+const Student = require ('../models/student.js');
 
 /**
  * @param {import('express').Request} req
  * @param {import('express').Response} res
  */
-async function postLogin (req, res) {
+async function postLogin(req, res) {
   // si l'utilisateur est déjà connecté, alors on lui retourne Unauthorized
   if (req.session.userId) {
     res.status(401)
@@ -18,6 +19,7 @@ async function postLogin (req, res) {
 
   // on vérifie l'email et le mot de passe de l'utilisateur
   const teacher = await Teacher.verifyTeacher(email, password);
+  const student = await Student.verifyStudent(email, password);
   // si on a trouvé un utilisateur correspondant, alors on sauvegarde son ID
   // dans l'objet req.session
   if (teacher) {
@@ -25,10 +27,20 @@ async function postLogin (req, res) {
     res.json(teacher);
     return;
   }
-
+  
+  if (student) {
+    console.log("student")
+    req.session.userId = student.id;
+    res.json(student);
+    return;
+  }
   // si on n'a pas trouvé d'utilisateur, alors on retourne une erreur 401 (unauthorized)
   res.status(401)
     .send('Unknown email or password');
+
+
+  
 }
+
 
 module.exports = postLogin;
--- backend/models/student.js
+++ backend/models/student.js
@@ -1,7 +1,9 @@
 const PostgresStore = require('../utils/PostgresStore.js');
+const bcrypt = require('bcrypt');
+
 
 class student {
-  static toSqlTable () {
+  static toSqlTable() {
     return ` 
           CREATE TABLE ${student.tableName}(
           id SERIAL PRIMARY KEY,
@@ -17,21 +19,46 @@ class student {
       )`;
   }
 
-  static async insert (json) {
+  static async insert(json) {
+    const hashedPassword = await bcrypt.hash(json.password,10);
     const result = await PostgresStore.pool.query({
       text:
-      `
+        `
       INSERT INTO ${student.tableName}
       (firstname,lastname,email,password,pseudo,type,idclasses)
       VALUES($1,$2,$3,$4,$5,$6,$7)      
 
       `,
-      values: [json.firstname, json.lastname, json.email, json.password, json.pseudo, json.type, json.idclasses]
+      values: [json.firstname, json.lastname, json.email, hashedPassword, json.pseudo, json.type, json.idclasses]
     });
     return result;
+  }
+  static async verifyStudent(mail, password) {
+    const result = await PostgresStore.pool.query({
+      text: ` SELECT * FROM ${student.tableName}
+            WHERE email like $1 
+    `,
+      values: [mail]
+    });
+    console.log(result);
+    if (result.rows.length === 0) {
+      return null;
+    }
+
+    const currentPassword = result.rows[0].password;
+
+    const isSame = await bcrypt.compare(password, currentPassword);
+    if (isSame) {
+      console.log('gg');
+      const student = result.rows[0];
+      delete student.password;
+      return student;
+    } else {
+      console.log('pas gg');
+      return null;
+    }
   }
 }
 student.tableName = 'student';
 
-module.exports = student
-;
+module.exports = student;
