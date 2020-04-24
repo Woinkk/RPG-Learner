23a8742c8ff6c4c7853ed3b17d3f7d8b3a90ee96
Woinkk <Darkrai99@live.Fr>
2020-04-24 09:26:42 +0200

    Début de la page d'accueil des profs

---
+++ frontend/src/components/AddSubject.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="text-center">
+      <v-dialog
+        v-model="dialog"
+        width="500"
+      >
+        <template v-slot:activator="{ on }">
+          <v-btn class="mx-2" fab small dark color="indigo" v-on="on">
+            <v-icon dark>mdi-plus</v-icon>
+        </v-btn>
+        </template>
+  
+        <v-card>
+          <v-card-title
+            class="headline grey lighten-2"
+            primary-title
+          >
+            Créer un sujet
+          </v-card-title>
+  
+          <v-card-text>
+            Nom du sujet: <v-text-field></v-text-field>
+          </v-card-text>
+  
+          <v-divider></v-divider>
+  
+          <v-card-actions>
+            <v-spacer></v-spacer>
+            <v-btn
+              color="primary"
+              text
+              @click="dialog = false"
+            >
+              Créer
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "AddSubject",
+    data () {
+        return {
+        dialog: false,
+        }
+    }
+}
+</script>
+
+<style>
+
+</style>script
--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,11 +1,157 @@
 <template>
-        <div>aaa</div>
+  <v-container>
+    <v-row dense>
+      <v-col cols="12">
+        <v-card color="#295665" dark>
+          <v-card-title class="headline">Créer un quizz</v-card-title>
+
+          <v-card-subtitle>Un quizz est une série de question.</v-card-subtitle>
+
+          <v-card-actions>
+            <v-menu open-on-hover top offset-y transition="fab-transition" :elevation="20">
+              <template v-slot:activator="{ on }">
+                <v-btn color="primary" dark v-on="on" v-if="selectedMatiere === null">Matière</v-btn>
+                <v-btn color="primary" dark v-on="on" v-else>{{selectedMatiere}}</v-btn>
+              </template>
+
+              <v-list>
+                <v-list-item
+                  v-for="(item, index) in matiere"
+                  :key="index"
+                  @click="selectMatiere(item.name)"
+                >
+                  <v-list-item-title>{{ item.name }}</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+            <span style="display:inline-block; width: 60px;"></span>
+            <v-menu open-on-hover top offset-y transition="fab-transition">
+              <template v-slot:activator="{ on }">
+                <v-btn color="primary" dark v-on="on" v-if="selectedSubject === null">Sujet</v-btn>
+                <v-btn color="primary" dark v-on="on" v-else>{{selectedSubject}}</v-btn>
+              </template>
+
+              <v-list>
+                <v-list-item
+                  v-for="(item, index) in subject"
+                  :key="index"
+                  @click="selectSubject(item.name)"
+                >
+                  <v-list-item-title>{{ item.name }}</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+            <AddSubject></AddSubject>
+            <v-spacer></v-spacer>
+            <v-btn>Créer</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+      <v-col cols="12">
+        <v-card color="#4c727e" dark>
+          <v-card-title class="headline">Classe VS Classe</v-card-title>
+
+          <v-card-subtitle>Permet de créer un affrontement entre classe</v-card-subtitle>
+
+          <v-card-actions>
+            <v-menu open-on-hover top offset-y transition="fab-transition">
+              <template v-slot:activator="{ on }">
+                <v-btn color="primary" dark v-on="on" v-if="selectedMyClasses === null">Ma classe</v-btn>
+                <v-btn color="primary" dark v-on="on" v-else>{{selectedMyClasses}}</v-btn>
+              </template>
+
+              <v-list>
+                <v-list-item
+                  v-for="(item, index) in myClasses"
+                  :key="index"
+                  @click="selectMyClasses(item.name)"
+                >
+                  <v-list-item-title>{{ item.name }}</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+            <span style="display:inline-block; width: 60px;"></span>
+            <v-menu open-on-hover top offset-y transition="fab-transition">
+              <template v-slot:activator="{ on }">
+                <v-btn color="primary" dark v-on="on" v-if="selectedAllClasses === null">Autre classe</v-btn>
+                <v-btn color="primary" dark v-on="on" v-else>{{selectedAllClasses}}</v-btn>
+              </template>
+
+              <v-list>
+                <v-list-item
+                  v-for="(item, index) in allClasses"
+                  :key="index"
+                  @click="selectAllClasses(item.name)"
+                >
+                  <v-list-item-title>{{ item.name }}</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+            <v-spacer></v-spacer>
+            <v-btn>Créer</v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-col>
+    </v-row>
+  </v-container>
 </template>
 
 
 <script>
+import AddSubject from "./AddSubject.vue";
 export default {
-    name:'Home',
-
-}
+  name: "Home",
+  components: {
+    AddSubject
+  },
+  data: () => ({
+    matiere: [
+      {
+        name: "mathématique"
+      }
+    ],
+    subject: [
+      {
+        name: "multiplication"
+      },
+      {
+        name: "addition"
+      }
+    ],
+    myClasses: [
+        {
+            name: "classe 4A"
+        }
+    ],
+    allClasses: [
+        {
+            name: "classe 5C"
+        },
+        {
+            name: "classe 2B"
+        }, 
+        {
+            name: "classe 3A"
+        }
+    ],
+    selectedMatiere: null,
+    selectedSubject: null,
+    selectedMyClasses: null,
+    selectedAllClasses: null
+  }),
+  methods: {
+    selectMatiere: function(matiere) {
+      this.selectedMatiere = matiere;
+    },
+    selectSubject: function(subject) {
+      this.selectedSubject = subject;
+    },
+    selectMyClasses: function(myClass) {
+      this.selectedMyClasses = myClass;
+    },
+    selectAllClasses: function(Class) {
+      this.selectedAllClasses = Class;
+    }
+  }
+};
 </script>
