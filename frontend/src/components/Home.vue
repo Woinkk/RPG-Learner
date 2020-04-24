dd4658f48450901cb26b1c58bb621fcfd7b77468
Woinkk <Darkrai99@live.Fr>
2020-04-24 10:41:52 +0200

    Created a vue component for CreateQuizz and ModifyQuizz

--- frontend/src/components/Home.vue
+++ frontend/src/components/Home.vue
@@ -1,57 +1,13 @@
 <template>
   <v-container>
     <v-row dense>
-      <v-col cols="12">
-        <v-card color="#295665" dark>
-          <v-card-title class="headline">Créer un quizz</v-card-title>
-
-          <v-card-subtitle>Un quizz est une série de question.</v-card-subtitle>
-
-          <v-card-actions>
-            <v-menu open-on-hover top offset-y transition="fab-transition" :elevation="20">
-              <template v-slot:activator="{ on }">
-                <v-btn color="primary" dark v-on="on" v-if="selectedMatiere === null">Matière</v-btn>
-                <v-btn color="primary" dark v-on="on" v-else>{{selectedMatiere}}</v-btn>
-              </template>
-
-              <v-list>
-                <v-list-item
-                  v-for="(item, index) in matiere"
-                  :key="index"
-                  @click="selectMatiere(item.name)"
-                >
-                  <v-list-item-title>{{ item.name }}</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-menu>
-            <span style="display:inline-block; width: 60px;"></span>
-            <v-menu open-on-hover top offset-y transition="fab-transition">
-              <template v-slot:activator="{ on }">
-                <v-btn color="primary" dark v-on="on" v-if="selectedSubject === null">Sujet</v-btn>
-                <v-btn color="primary" dark v-on="on" v-else>{{selectedSubject}}</v-btn>
-              </template>
-
-              <v-list>
-                <v-list-item
-                  v-for="(item, index) in subject"
-                  :key="index"
-                  @click="selectSubject(item.name)"
-                >
-                  <v-list-item-title>{{ item.name }}</v-list-item-title>
-                </v-list-item>
-              </v-list>
-            </v-menu>
-            <AddSubject></AddSubject>
-            <v-spacer></v-spacer>
-            <v-btn>Créer</v-btn>
-          </v-card-actions>
-        </v-card>
-      </v-col>
+      <CreateQuizz @switchMode= "switchToModification" v-if="CreationMode === true"></CreateQuizz>
+      <ModifyQuizz @switchMode= "switchToCreation" v-else></ModifyQuizz>
       <v-col cols="12">
         <v-card color="#4c727e" dark>
           <v-card-title class="headline">Classe VS Classe</v-card-title>
 
-          <v-card-subtitle>Permet de créer un affrontement entre classe</v-card-subtitle>
+          <v-card-subtitle>Permet de créer un affrontement entre classe.</v-card-subtitle>
 
           <v-card-actions>
             <v-menu open-on-hover top offset-y transition="fab-transition">
@@ -98,26 +54,16 @@
 
 
 <script>
-import AddSubject from "./AddSubject.vue";
+import CreateQuizz from "./CreateQuizz.vue";
+import ModifyQuizz from "./ModifyQuizz.vue";
 export default {
   name: "Home",
   components: {
-    AddSubject
+    CreateQuizz,
+    ModifyQuizz
   },
   data: () => ({
-    matiere: [
-      {
-        name: "mathématique"
-      }
-    ],
-    subject: [
-      {
-        name: "multiplication"
-      },
-      {
-        name: "addition"
-      }
-    ],
+    CreationMode: true,
     myClasses: [
         {
             name: "classe 4A"
@@ -134,23 +80,22 @@ export default {
             name: "classe 3A"
         }
     ],
-    selectedMatiere: null,
-    selectedSubject: null,
     selectedMyClasses: null,
-    selectedAllClasses: null
+    selectedAllClasses: null,
+
   }),
   methods: {
-    selectMatiere: function(matiere) {
-      this.selectedMatiere = matiere;
-    },
-    selectSubject: function(subject) {
-      this.selectedSubject = subject;
-    },
     selectMyClasses: function(myClass) {
       this.selectedMyClasses = myClass;
     },
     selectAllClasses: function(Class) {
       this.selectedAllClasses = Class;
+    },
+    switchToModification: function () {
+      this.CreationMode = false;
+    },
+    switchToCreation: function () {
+      this.CreationMode = true;
     }
   }
 };
