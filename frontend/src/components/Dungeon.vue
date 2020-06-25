464ca941b48351378db0ea90d5d1ab231783d719
Woinkk <Darkrai99@live.Fr>
2020-06-25 09:08:58 +0200

    début des donjons

---
+++ frontend/src/components/Dungeon.vue
@@ -0,0 +1,26 @@
+<template>
+    <v-container>
+        
+    </v-container>
+</template>
+
+
+<script>
+export default {
+  name: "Dungeon",
+  components: {
+      
+  },
+  data: () => ({
+    idEleve: "",
+    quizz: [],
+    answer: [],
+  }),
+  methods: {
+    
+  },
+  mounted: {
+      
+  }
+};
+</script>
